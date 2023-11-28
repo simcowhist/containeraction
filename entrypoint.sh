d8454c439695d130c959b61da752475ea9088321
simcowhist <152062780+simcowhist@users.noreply.github.com>
2023-11-28 09:23:23 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "HELLO $INPUT_MYINPUT"
-MEMORY=$(cat /proc/meminfo)
-echo "::set-output name=MEMORY::$MEMORY"
+memory=$(cat /proc/meminfo)
+echo "::set-output name=memory::$memory"
